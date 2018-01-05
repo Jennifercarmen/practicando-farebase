0e7c383b60c66f90e50a149e15da82ac54217e56
Jennifercarmen <jennifercarmens@gmail.com>
2018-01-05 11:57:32 -0500

    funcionando

--- js/app.js
+++ js/app.js
@@ -1,32 +1,27 @@
-;(function(){
-// Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCNcFyocem97d1fF_ftgMG8j-XQWgUIjQY",
-  authDomain: "laboratoria-test-a71b4.firebaseapp.com",
-  databaseURL: "https://laboratoria-test-a71b4.firebaseio.com",
-  projectId: "laboratoria-test-a71b4",
-  storageBucket: "laboratoria-test-a71b4.appspot.com",
-  messagingSenderId: "1002692745055"
+  apiKey: "AIzaSyDz6Bsp6T9TuR7gYIg5i-tTScsbIFeVyYo",
+  authDomain: "laboratoria-2a397.firebaseapp.com",
+  databaseURL: "https://laboratoria-2a397.firebaseio.com",
+  projectId: "laboratoria-2a397",
+  storageBucket: "laboratoria-2a397.appspot.com",
+  messagingSenderId: "617399103792"
 };
+
 firebase.initializeApp(config);
 
+var $btn = $('#butto');
 
-$btn = $('#butto');
-
-$user = null;
+var $user = null;
 
 function signInGoogle() {
   console.log('hola')
   var provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth().signInWithPopup(provider).then(function (result) {
-    var user = result.user;
-    // ...
+    $user = result.user;
+
     console.log($user);
   })
 }
 
-
 $btn.on('click', signInGoogle);
-
-})();
